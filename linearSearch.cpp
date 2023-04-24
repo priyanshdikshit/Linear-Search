b36737420d780de76e533550ed7d395a51bed2ae
priyanshdikshit <125885286+priyanshdikshit@users.noreply.github.com>
2023-04-24 21:47:51 +0530

    Add files via upload

---
+++ linearSearch.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int linear_search(int arr[], int n, int target) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == target) {
+            return i; // Return the index if target found
+        }
+    }
+    return -1; // Return -1 if target not found
+}
+
+int main() {
+    int arr[] = {3, 8, 4, 1, 9, 5, 7};
+    int n = 7 // sizeof(arr) / sizeof(arr[0]);
+    int target = 5;
+    int result = linear_search(arr, n, target);
+    if (result == -1) {
+        printf("Target value not found in the array.\n");
+    } else {
+        printf("Target value found at index: %d\n", result);
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
